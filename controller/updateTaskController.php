4a34fe38b7c06ac8ddca79b35b35a49fb2c297ce
Bbop311 <bbops311@gmail.com>
2024-06-11 16:58:22 +0200

    end of day 06.05

--- controller/updateTaskController.php
+++ controller/updateTaskController.php
@@ -5,6 +5,12 @@ include ('../config.php');
 $pdo = connect_db();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["update_task"])) {
+    $data_array = ['id' => $_POST['id']];
+    $data_array = ['name' => $_POST['name']];
+    $data_array = ['name' => $_POST['name']];
+    $data_array = ['name' => $_POST['name']];
+    $data_array = ['name' => $_POST['name']];
+    $data_array = ['name' => $_POST['name']];
     $data_array = ['name' => $_POST['name']];
     updateTask($pdo, $id, $data_array);
 }
