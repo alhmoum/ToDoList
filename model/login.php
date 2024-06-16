179bad322863c795e8e5903ea3ba60658852c391
Bbop311 <bbops311@gmail.com>
2024-06-16 22:01:08 +0200

    add secure sign up and login model view and controller

--- model/login.php
+++ model/login.php
@@ -1,20 +1,29 @@
 <?php
 
-require ('../config.php');
+require('../config.php');
 
-function loginConfirmation ($login, $password){
-    $pdo = connect_db();
-    $sql = "SELECT password FROM users WHERE first_name = ?";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute([$login]);
-    $user = $stmt->fetch();
-    /* Attention, il faut que les mots de passe soit hashé pour que la confirmation fonctionne */
-    if ($user && password_verify($password, $user['password'])){
-        $_SESSION['user_id'] = $user['id'];
-        header("Location; index.php");
+function loginConfirmation($login, $password)
+{
+    try {
+        /* Connexion à la base de données */
+        $pdo = connect_db();
+        /* Préparation de la requête SQL */
+        $sql = "SELECT password FROM users WHERE username = :login";
+        /* préparation de la requête */
+        $stmt = $pdo->prepare($sql);
+        /* Execution de la requête avec en argument un tableau contenant pour seul élément le login */
+        $stmt->execute(array('login' => $login));
+        /* On fetch la première (et donc unique dans ce cas) ligne renvoyée par la requête */
+        $user = $stmt->fetch();
+        /* Password_verify permet de comparer le mot de passe entré par l'utilisateur (non hashé)
+        avec le hash stocké en BDD */
+        if ($user && password_verify($password, $user['password'])) {
+            echo '<div>connexion réussie !</div>';
+            $_SESSION['user_id'] = $user['id'];
+        } else {
+            echo "Nom d'utilisateur ou mot de passe incorrect.";
+        }
+    } catch (Exception $e) {
+        die ('Erreur : '.$e->getMessage());
     }
-    else{
-        echo "Nom d'utilisateur ou mot de passe incorrect.";
-    }
-
 }
